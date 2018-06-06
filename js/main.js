30bb4084f6937c289af5dc87d8248660a9882543
mattvernon <hello@matthewvernon.co>
2018-06-06 13:31:52 +1000

    finished up site

--- js/main.js
+++ js/main.js
@@ -53,3 +53,19 @@ $('a[href*="#"]')
       }
     }
   })
+
+
+// progress bar ============
+
+$(document).on('scroll', function() {
+  var pixelsFromTop = $(document).scrollTop();
+  var documentHeight = $(document).height()
+  var windowHeight = $(window).height()
+
+  var difference = documentHeight - windowHeight
+
+  var percentage = 100 * pixelsFromTop / difference
+
+  $('#progress-bar').css('width', percentage + '%')
+
+});
